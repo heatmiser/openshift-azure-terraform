b80ffa21c51fe4737e903f3feda73931ac671115
Mike Savage <mike.savage@gmail.com>
2018-05-30 21:25:05 -0400

    Create log file with .log extension

--- ansible-inv/main.tf
+++ ansible-inv/main.tf
@@ -31,12 +31,12 @@ data "terraform_remote_state" "ocpinfra" {
   }
 }
 
-resource "local_file" "ansible_inventory_out" {
+resource "local_file" "ansible_inventory_log" {
   content  = ""
-  filename = "${path.module}/ansible_inventory_out"
+  filename = "${path.module}/ansible_inventory.log"
 
   provisioner "local-exec" {
-    command = "bash deployOpenShift_cri-o.sh \"${var.admin_username}\" \"${var.openshift_password}\" \"${var.project}-master\" \"${data.terraform_remote_state.ocpmaster.openshift_master_pip_fqdn}\" \"${data.terraform_remote_state.ocpmaster.openshift_master_pip_ipaddr}\" \"${var.project}-infra\" \"${var.project}-node\" \"${var.node_instance_count}\" \"${var.infra_instance_count}\" \"${var.master_instance_count}\" \"${data.terraform_remote_state.ocpinfra.openshift_infra_load_balancer_ipaddr}.nip.io\" \"${data.terraform_remote_state.ocpinfra.registry_storage_account_name}\" \"${data.terraform_remote_state.ocpinfra.registry_storage_account_primary_access_key}\" \"${var.enableMetrics}\" \"${var.enableLogging}\" \"${var.tenant_id}\" \"${var.subscription_id}\" \"${var.aad_client_id}\" \"${var.aad_client_secret}\" \"${data.terraform_remote_state.base.resource_group_name}\" \"${data.terraform_remote_state.base.location}\" \"${var.enableCockpit}\"  \"${var.enableAzure}\" \"${var.storageKind}\" \"${var.enableCRS}\" \"${var.project}-${var.ocpcrs_app_module_name}\" \"${var.crsapp_instance_count}\" \"${var.project}-${var.ocpcrs_registry_module_name}\" \"${var.crsreg_instance_count}\" \"${var.gluster_disk_count}\" > \"${local_file.ansible_inventory_out.filename}\""
+    command = "bash deployOpenShift_cri-o.sh \"${var.admin_username}\" \"${var.openshift_password}\" \"${var.project}-master\" \"${data.terraform_remote_state.ocpmaster.openshift_master_pip_fqdn}\" \"${data.terraform_remote_state.ocpmaster.openshift_master_pip_ipaddr}\" \"${var.project}-infra\" \"${var.project}-node\" \"${var.node_instance_count}\" \"${var.infra_instance_count}\" \"${var.master_instance_count}\" \"${data.terraform_remote_state.ocpinfra.openshift_infra_load_balancer_ipaddr}.nip.io\" \"${data.terraform_remote_state.ocpinfra.registry_storage_account_name}\" \"${data.terraform_remote_state.ocpinfra.registry_storage_account_primary_access_key}\" \"${var.enableMetrics}\" \"${var.enableLogging}\" \"${var.tenant_id}\" \"${var.subscription_id}\" \"${var.aad_client_id}\" \"${var.aad_client_secret}\" \"${data.terraform_remote_state.base.resource_group_name}\" \"${data.terraform_remote_state.base.location}\" \"${var.enableCockpit}\"  \"${var.enableAzure}\" \"${var.storageKind}\" \"${var.enableCRS}\" \"${var.project}-${var.ocpcrs_app_module_name}\" \"${var.crsapp_instance_count}\" \"${var.project}-${var.ocpcrs_registry_module_name}\" \"${var.crsreg_instance_count}\" \"${var.gluster_disk_count}\" > \"${local_file.ansible_inventory_log.filename}\""
   }
 }
 
