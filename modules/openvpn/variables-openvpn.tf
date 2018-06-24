8f1e836bb8f6a993fcf13fe2bd619bd4d13381f8
Mike Savage <mike.savage@gmail.com>
2018-06-24 01:37:15 -0400

    Initial commit

---
+++ modules/openvpn/variables-openvpn.tf
@@ -0,0 +1,15 @@
+variable "openvpn_vm_size" {}
+
+variable "crypto_algo_elliptical" {}
+
+variable "openvpn_client_cert_name" {}
+
+variable "openvpn_client_cert_state" {}
+
+variable "openvpn_client_cert_city" {}
+
+variable "openvpn_client_cert_org" {}
+
+variable "openvpn_client_cert_email" {}
+
+variable "openvpn_client_cert_ou" {}
