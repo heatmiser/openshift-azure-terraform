4782986ebf3718bc65a350e73ae02a887d3843e1
Mike Savage <mike.savage@gmail.com>
2018-07-06 03:47:04 -0400

    echo to check variables

--- packer-rhel7/scripts/base-rhel.sh
+++ packer-rhel7/scripts/base-rhel.sh
@@ -1,4 +1,7 @@
 # Register Red Hat Subscription
+echo "rhn_username_org set to $rhn_username_org"
+echo "rhn_password_act_keyset to $rhn_password_act_keyset"
+echo "rhn_pool_id set to $rhn_pool_id"
 subscription-manager register --username="$rhn_username_org" --password="$rhn_password_act_key" || subscription-manager register --activationkey="$rhn_password_act_key" --org="$rhn_username_org"
 if [ "$rhn_pool_id" != "null" ]; then
     subscription-manager attach --pool="$rhn_pool_id"
@@ -15,8 +18,7 @@ yum -y install ca-certificates
 
 # Only install cloud-init on "cloud-ci-" hostname systems
 
-if hostname -f|grep -e "cloud-ci-" >/dev/null
-then
+if hostname -f|grep -e "cloud-ci-" >/dev/null; then
    echo $(date) " - Installing cloud-init"
    subscription-manager repos --enable=rhel-7-server-rh-common-rpms
    yum -y install cloud-init
