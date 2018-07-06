775617784840274a12159395ae0254a4e346ffdc
Mike Savage <mike.savage@gmail.com>
2018-07-06 01:53:19 -0400

    Code fixes

--- packer-rhel7/pack_it.sh
+++ packer-rhel7/pack_it.sh
@@ -5,13 +5,17 @@ if command -v packer 2>/dev/null; then
     if [ "$testpacker" == "" ]; then
         export PACKER="$(command -v packer)"
     fi
-elif command -v ./packer 2>/dev/null; then
-    export PACKER="$(command -v ./packer)"
+fi
+if [ "$PACKER" == "" ]; then
+    if command -v ./packer 2>/dev/null; then
+        export PACKER="$(command -v ./packer)"
+    fi
 fi
 if [ "$PACKER" == "" ]; then
     echo "packer not found, downloading locally..."
     wget https://releases.hashicorp.com/packer/1.2.4/packer_1.2.4_linux_amd64.zip
     unzip packer_1.2.4_linux_amd64.zip
+    rm -f packer_1.2.4_linux_amd64.zip
     if command -v ./packer 2>/dev/null; then
         export PACKER="$(command -v ./packer)"
     else
@@ -19,14 +23,14 @@ if [ "$PACKER" == "" ]; then
         exit 1
     fi
 fi
-echo 'Using packer "$PACKER"'
-rhn_username_org=$(grep rhn_user ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')
-rhn_password_act_key=$(grep rhn_passwd ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')
-rhn_pool_id=$(grep rhn_poolid ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')
+echo "Using packer '$PACKER'"
+rhn_username_org="$(grep rhn_user ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')"
+rhn_password_act_key="$(grep rhn_passwd ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')"
+rhn_pool_id="$(grep rhn_poolid ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')"
 
 "$PACKER" build \
-    -var 'rhn_username_org=$rhn_username_org' \
-    -var 'rhn_password_act_key=$rhn_password_act_key' \
-    -var 'rhn_password_act_key=$rhn_pool_id' \
+    -var "rhn_username_org='$rhn_username_org'" \
+    -var "rhn_password_act_key='$rhn_password_act_key'" \
+    -var "rhn_pool_id='$rhn_pool_id'" \
     -only=rhel-7-cloud-azure \
     rhel7.json
