4aa70072c18b92c8d39d6ab40369c2263d762478
Mike Savage <mike.savage@gmail.com>
2018-07-06 01:33:40 -0400

    double quote where necessary

--- packer-rhel7/pack_it.sh
+++ packer-rhel7/pack_it.sh
@@ -2,28 +2,29 @@
 
 if command -v packer 2>/dev/null; then
     testpacker="$(packer --help 2>&1 | grep dbname)"
-    if [ $testpacker == "" ]; then
-        export PACKER=$(command -v packer)
+    if [ "$testpacker" == "" ]; then
+        export PACKER="$(command -v packer)"
     fi
 elif command -v ./packer 2>/dev/null; then
-    export PACKER=$(command -v ./packer)
-else
+    export PACKER="$(command -v ./packer)"
+fi
+if [ "$PACKER" == "" ]; then
     echo "packer not found, downloading locally..."
     wget https://releases.hashicorp.com/packer/1.2.4/packer_1.2.4_linux_amd64.zip
     unzip packer_1.2.4_linux_amd64.zip
     if command -v ./packer 2>/dev/null; then
-        export PACKER=$(command -v ./packer)
+        export PACKER="$(command -v ./packer)"
     else
         echo "Something went wrong, exiting..."
         exit 1
     fi
 fi
-
+echo 'Using packer "$PACKER"'
 rhn_username_org=$(grep rhn_user ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')
 rhn_password_act_key=$(grep rhn_passwd ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')
 rhn_pool_id=$(grep rhn_poolid ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')
 
-$PACKER build \
+"$PACKER" build \
     -var 'rhn_username_org=$rhn_username_org' \
     -var 'rhn_password_act_key=$rhn_password_act_key' \
     -var 'rhn_password_act_key=$rhn_pool_id' \
