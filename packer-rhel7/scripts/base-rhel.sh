16737aef97e379f024c939b364f227e06008087d
Mike Savage <mike.savage@gmail.com>
2018-07-06 04:27:24 -0400

    adjust debug echo output

--- packer-rhel7/scripts/base-rhel.sh
+++ packer-rhel7/scripts/base-rhel.sh
@@ -1,7 +1,7 @@
 # Register Red Hat Subscription
-echo "rhn_username_org set to $rhn_username_org"
-echo "rhn_password_act_keyset to $rhn_password_act_keyset"
-echo "rhn_pool_id set to $rhn_pool_id"
+echo "rhn_username_org     ==> $rhn_username_org"
+echo "rhn_password_act_key ==> $rhn_password_act_key"
+echo "rhn_pool_id          ==> $rhn_pool_id"
 subscription-manager register --username="$rhn_username_org" --password="$rhn_password_act_key" || subscription-manager register --activationkey="$rhn_password_act_key" --org="$rhn_username_org"
 if [ "$rhn_pool_id" != "null" ]; then
     subscription-manager attach --pool="$rhn_pool_id"
