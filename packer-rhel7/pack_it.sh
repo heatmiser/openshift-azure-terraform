08807095d66b5e5d8e226849e1c347da280eff63
Mike Savage <mike.savage@gmail.com>
2018-07-08 02:38:00 -0400

    packer should already be installed & pass RHN variables on command line

--- packer-rhel7/pack_it.sh
+++ packer-rhel7/pack_it.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#DEBUG="-debug"
+
 if command -v packer 2>/dev/null; then
     testpacker="$(packer --help 2>&1 | grep dbname)"
     if [ "$testpacker" == "" ]; then
@@ -12,34 +14,28 @@ if [ "$PACKER" == "" ]; then
     fi
 fi
 if [ "$PACKER" == "" ]; then
-    echo "packer not found, downloading locally..."
-    wget https://releases.hashicorp.com/packer/1.2.4/packer_1.2.4_linux_amd64.zip
-    unzip packer_1.2.4_linux_amd64.zip
-    rm -f packer_1.2.4_linux_amd64.zip
-    if command -v ./packer 2>/dev/null; then
-        export PACKER="$(command -v ./packer)"
-    else
-        echo "Something went wrong, exiting..."
-        exit 1
+    echo "packer not found, exiting..."
+    exit 1
     fi
 fi
-echo "Using packer '$PACKER'"
+echo "Using packer: '$PACKER'"
 rhn_username_org="$(grep rhn_user ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')"
 rhn_password_act_key="$(grep rhn_passwd ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')"
 rhn_pool_id="$(grep rhn_poolid ../env-dev/03rhn.tfvars | awk '{print $3}' | tr -d '"')"
 
+echo ""
 echo "Executing:"
 echo ""
-echo "$PACKER build \\"
-echo "    -var 'rhn_username_org=$rhn_username_org' \\"
-echo "    -var 'rhn_password_act_key=$rhn_password_act_key' \\"
-echo "    -var 'rhn_pool_id=$rhn_pool_id' \\"
+echo "rhn_username_org=$rhn_username_org \\"
+echo "rhn_password_act_key=$rhn_password_act_key \\"
+echo "rhn_pool_id=$rhn_pool_id \\"
+echo "$PACKER build $DEBUG\\"
 echo "    -only=rhel-7-cloud-azure \\"
 echo "    rhel7.json"
 
-"$PACKER" build \
-    -var \'rhn_username_org=$rhn_username_org\' \
-    -var \'rhn_password_act_key=$rhn_password_act_key\' \
-    -var \'rhn_pool_id=$rhn_pool_id\' \
+rhn_username_org="$rhn_username_org" \
+rhn_password_act_key="$rhn_password_act_key" \
+rhn_pool_id="$rhn_pool_id" \
+"$PACKER" build $DEBUG \
     -only=rhel-7-cloud-azure \
     rhel7.json
