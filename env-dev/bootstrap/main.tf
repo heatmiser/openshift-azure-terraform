d0a5b0d2c734cdd371c2538533220f1e7e6e7026
Mike Savage <mike.savage@gmail.com>
2019-04-18 16:35:18 -0400

    Remove duplicates plus cleanup

--- env-dev/bootstrap/main.tf
+++ env-dev/bootstrap/main.tf
@@ -1,18 +1,13 @@
 module "bootstrap" {
   source = "../../modules/bootstrap"
 
-  resource_group_name  = "${var.resource_group_name}"
-  storage_account_name = "${var.storage_account_name}"
-
-  #key                         = "${var.key}"
-  #container_name              = "${var.container_name}"
-  project = "${var.project}"
-
-  environment          = "${var.environment}"
-  env_version          = "${var.env_version}"
-  location             = "${var.location}"
-  network1_module_name = "${var.network1_module_name}"
-
+  resource_group_name         = "${var.resource_group_name}"
+  storage_account_name        = "${var.storage_account_name}"
+  project                     = "${var.project}"
+  environment                 = "${var.environment}"
+  env_version                 = "${var.env_version}"
+  location                    = "${var.location}"
+  network1_module_name        = "${var.network1_module_name}"
   network2_module_name        = "${var.network2_module_name}"
   ocpmaster_module_name       = "${var.ocpmaster_module_name}"
   ocpinfra_module_name        = "${var.ocpinfra_module_name}"
@@ -20,7 +15,6 @@ module "bootstrap" {
   ocpbastion_module_name      = "${var.ocpbastion_module_name}"
   ocpcrs_app_module_name      = "${var.ocpcrs_app_module_name}"
   ocpcrs_registry_module_name = "${var.ocpcrs_registry_module_name}"
-  ocpopenvpn_module_name      = "${var.ocpopenvpn_module_name}"
   ocpopenvpn_module_name      = "${var.ocpopenvpn_module_name}"
   gluster_disk_count          = "${var.gluster_disk_count}"
 }
